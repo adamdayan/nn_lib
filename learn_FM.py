a352a04609588d18319f0f8a07b49bb014f7ea3d
Robert Lewis <rl617@edge07.doc.ic.ac.uk>
2019-02-26 19:17:52 +0000

    basic preprocessing

--- learn_FM.py
+++ learn_FM.py
@@ -5,33 +5,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import matplotlib.pyplot as plt
-
-from torch_utils import *
-
-from nn_lib import (
-    MultiLayerNetwork,
-    Trainer,
-    Preprocessor,
-    save_network,
-    load_network,
-)
+from torch.utils.data import Dataset, DataLoader
 from illustrate import illustrate_results_FM
- 
+from pytorch_pp import TorchPreprocessor
+
+
+class FMDataset(Dataset):
+
+    # Initialize your data, download, etc.
+    def __init__(self, data):
+        self.dataset = data
+        self.len = self.dataset.shape[0]
+        self.x_data = torch.from_numpy(self.dataset[:,:3])
+        self.y_data = torch.from_numpy(self.dataset[:,3:])
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.len
+
 def main():
     dataset = np.loadtxt("FM_dataset.dat")
     #######################################################################
     #                       ** START OF YOUR CODE **
     #######################################################################
 
-    model_path = "output/learn_fm/20190226_111420/"
-    model = load_torch_model(model_path + "20190226_111420_model_model.pt", model_path + "20190226_111420_model_layers.pickle")
-
-    print(model.forward(dataset[:, :3]))
-    
-    #######################################################################
-    #                       ** END OF YOUR CODE **
-    #######################################################################
+    # #######################################################################
+    # #                       ** END OF YOUR CODE **
+    # #######################################################################
     # illustrate_results_FM(network, prep)
 
 
@@ -61,5 +63,5 @@ if __name__ == "__main__":
     # plt.ylabel("Loss (" + loss_fun + ")")
 
     # plt.savefig(loss_fun + "_loss_plot.png")
-    
+
     # out = network.forward(dataset
